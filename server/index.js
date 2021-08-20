7fc75a9610ba8a5b965c0bad658faf1f16120e71
Carston Work <carston.work@gmail.com>
2021-08-20 11:49:22 -0600

    I finished the MVP of what I wanted to do, now I'm going to work on an edit button for my good deeds (A put request).

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+memories = [];
+goals = [];
+
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
@@ -21,5 +24,62 @@ app.get("/api/compliment", (req, res) => {
   res.status(200).send(randomCompliment);
   
 });
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = [
+    "By the pricking of my thumbs, something wicked this way comes.",
+    "Don't be surprised if the end comes later than you expect.",
+    "The one with the power to vanquish the Dark Lord will be born as the seventh month dies...",
+    "When the time comes and you need a weapon, look under the roots of the Menoa Tree. Then, when all seems lost and your power is insufficient, go to the rock of Kuthian and speak your name to open the Vault of Souls.",
+    "Beware the Ides of March!"
+  ];
+
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
+});
+
+let nextGoalId = 1;
+let nextMemId = 1;
+
+app.post("/api/goals/", (req, res) => {
+  console.log(req.body)
+  const newGoalText = req.body.goal;
+  const newGoal = {
+    id: nextGoalId,
+    goal: newGoalText
+  }
+  nextGoalId++;
+  goals.push(newGoal);
+  console.log(goals);
+  res.status(200).send(goals);
+})
+
+app.delete("/api/goals/:id", (req, res) => {
+  console.log('delete request')
+  console.log(req.params.id)
+  const index = goals.findIndex(elem => elem.id === +req.params.id);
+  console.log(index)
+  const removed = goals.splice(index,1);
+  res.status(200).send(removed)
+  console.log(goals)
+});
+
+app.post('/api/memories', (req, res) => {
+  let newMem = {
+    id: nextMemId,
+    date : req.body.date,
+    memory: req.body.memory
+  };
+  memories.push(newMem)
+  nextMemId++
+  res.status(200).send(newMem)
+});
+
+app.get('/api/memories/:date', (req, res) => {
+  const filteredMems = memories.filter(elem => elem.date === req.params.date);
+  res.status(200).send(filteredMems);
+})
 
 app.listen(4000, () => console.log("Server running on 4000"));
